041722e111d9e10db88840c8726b6ad9ba20b9aa
teserax <steeles80@gmail.com>
2023-02-05 12:03:27 +0200

    calculating command line arguments that are real numbers

--- FirstPart/task1.go
+++ FirstPart/task1.go
@@ -1,3 +1,5 @@
+/*Напишите программу кторая вычисляет сумму всех аргументов командной строки,
+которые являются действительными числами*/
 package main
 
 import (
@@ -7,17 +9,21 @@ import (
 )
 
 func main() {
-	total := 0
-	if len(os.Args) == 1 {
+
+	if len(os.Args) == 1 { //если длина переданных аргументов ровна "1" это означает что нам ничего не передали так
+		// как первое значение это спец значение оно идет по умолчанию
 		fmt.Println("please give me one argument!")
-	} else {
-		for i := 1; i < len(os.Args); i++ {
-			number, err := strconv.Atoi(os.Args[i])
-			if err != nil {
-				fmt.Println(err)
-			}
-			total += number
-		}
 	}
-	fmt.Println(total)
+	total := 0
+	for i := 1; i < len(os.Args); i++ { //если передали.... то используя цикл for мы передаем полученные аргумент в переменную number
+		//и спользуя метотд пакета-strconv.Atoi конвертируем полученные данные в число так как  мы получаем их виде string
+		number, err := strconv.Atoi(os.Args[i])
+		if err != nil { //проверяем на корректность данных
+			fmt.Println(err)
+			return //если получим ошибку выводим в консоль и выходи из программы
+		}
+		total += number //суммируем
+	}
+
+	fmt.Println(total) //выводим полученный результат в консоль
 }
