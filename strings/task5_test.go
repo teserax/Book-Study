8dd8f1e830fcbcd007f9784bcf283b4ec98bb434
teserax <steeles80@gmail.com>
2023-02-27 00:09:07 +0200

    palidrom

---
+++ strings/task5_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestPalidrom(t *testing.T) {
+	var tests = []struct {
+		text string
+		pali bool
+	}{
+		{"топот", true},
+		{"wert", false},
+	}
+	for _, test := range tests {
+		if got := palidrom(test.text); got == test.pali {
+			t.Errorf(`Error!  want: %v  got: %v`, test.pali, got)
+		}
+	}
+
+}
