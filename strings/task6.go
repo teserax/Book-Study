c14e0ec4f0e3c4a2b6465f41a07c46f0744d64be
teserax <67872659+teserax@users.noreply.github.com>
2023-02-28 17:02:40 +0200

    iterator

---
+++ strings/task6.go
@@ -0,0 +1,31 @@
+//Даны 5 массивов символов, представляющих фрагмент текста программы.
+//Известно, что данный фрагмент оперирует только с целочисленными переменными
+//В каждой строке-одна команда
+//Преобразовать данный тест заменив каждую строку вида
+//          /переменная=переменная "+1" на строку "++"/
+// А каждую строку вида
+//          /переменная=переменная "-1" на строку переменная "--"/
+
+package main
+
+import "fmt"
+
+func iterator(text string) string {
+	if len(text) <= 1 {
+		return text
+	}
+	for i := 0; i < len(text); i++ {
+		if string(text[i]) == "=" && string(text[i+1]) == "+" && string(text[i+2]) == "1" {
+			text = text[:i] + "++" + text[i+3:]
+		}
+		if string(text[i]) == "=" && string(text[i+1]) == "-" && string(text[i+2]) == "1" {
+			text = text[:i] + "--" + text[i+3:]
+		}
+
+	}
+	return text
+}
+func main() {
+	text := " c=5;b=-1 = ++ a=+1"
+	fmt.Println(iterator(text))
+}
