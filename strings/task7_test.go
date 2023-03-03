acaa1013cbe2bba50edd1c1a4cb7f40ee59537df
teserax <67872659+teserax@users.noreply.github.com>
2023-03-03 08:40:45 +0200

    test reversText

--- strings/task7_test.go
+++ strings/task7_test.go
@@ -1,24 +1,35 @@
+/*
+Ведите массив символов из 17 элементов, содержащих два или более слов,
+разделенных пробелами. Поменять местами все четные и не четные слова в массиве
+*/
+
 package main
 
 import (
 	"fmt"
-	"testing"
+	"strings"
 )
 
-func TestReversText(t *testing.T) {
-	var test = []struct {
-		testText   string
-		returnText string
-	}{
-		{"first second", "second first"},
-	}
-
-	for _, test := range test {
-		if got := reversText(test.testText); got != test.returnText {
-			fmt.Printf("%v %v", got, test.returnText)
-			t.Errorf(`Error!  want: %v  got: %v`, test.returnText, got)
+func reversText(text string) string {
+	splitText := strings.Split(text, " ")
+	var result string
+	for i := 0; i < len(splitText); i++ {
+		if i+1 == len(splitText) {
+			result = " " + result + splitText[i]
+		} else {
+			result = result + splitText[i+1] + " " + splitText[i] + " "
+			i++
 		}
 
 	}
+	result = strings.TrimSpace(result)
+	return result
+}
+func main() {
 
+	//text := "Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, unde omnis iste natus error"
+	//	text := "first second first second first second first second"
+	text := "первый второй первый второй"
+	fmt.Println(len(text), len(reversText(text)))
+	fmt.Println(text, reversText(text))
 }
